0e79a7ecaa8d2fe62d9eecdec7be4f20dd96283c
Artem Kajalainen <artem.kajalainen@gofore.com>
2018-01-29 17:33:00 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear() + 1;
+});
+
+app.use((req,res, next) => {
+    var log = `${new Date().toString()}: method: ${req.method}, url: ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req,res,next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(`${__dirname}/public`));
+
+hbs.registerHelper('screamIt', (input) => {
+    return input.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs',{
+        welcomeMessage: "Превед, медвед!!1!",
+        pageTitle: 'Хомяк'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    })
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Err message'
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
